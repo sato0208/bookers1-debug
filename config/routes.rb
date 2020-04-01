b7c17fb943c4e3e3ea9f272f7066368f727cfa0f
sato chiharu <kujira028@gmail.com>
2020-04-01 04:47:46 +0000

    課題1

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	root 'homes#top'
+   get 'homes/top' => 'homes#top'
+
+  resources :books
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
